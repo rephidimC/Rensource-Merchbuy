d85fca28fddd34b458305d28e28d42e94696b836
Victor Adeleke AFOLAYAN <adelekeafolayan@gmail.com>
2021-05-31 19:30:17 +0100

    Added and modified files

---
+++ src/business-details.js
@@ -0,0 +1,64 @@
+const details = [
+    {
+        id: 1,
+        src: "/coke.jpg",
+        threeCardsText1: "paragraph1",
+        threeCardsText2: "paragraph2",
+        threeCardsText3: "paragraph3",
+        text1: "Free sample small tote bag gucci fed...",
+        text2: "#900 - #1,500",
+        text3: "MOQ 4 (pieces)"
+    },
+    {
+        id: 2,
+        src: "/coke.jpg",
+        threeCardsText1: "paragraph1",
+        threeCardsText2: "paragraph2",
+        threeCardsText3: "paragraph3",
+        text1: "Free sample small tote bag gucci fed...",
+        text2: "#900 - #1,500",
+        text3: "MOQ 4 (pieces)"
+    },
+    {
+        id: 3,
+        src: "/coke.jpg",
+        threeCardsText1: "paragraph1",
+        threeCardsText2: "paragraph2",
+        threeCardsText3: "paragraph3",
+        text1: "Free sample small tote bag gucci fed...",
+        text2: "#900 - #1,500",
+        text3: "MOQ 4 (pieces)"
+    },
+    {
+        id: 4,
+        src: "/coke.jpg",
+        threeCardsText1: "paragraph1",
+        threeCardsText2: "paragraph2",
+        threeCardsText3: "paragraph3",
+        text1: "Free sample small tote bag gucci fed...",
+        text2: "#900 - #1,500",
+        text3: "MOQ 4 (pieces)"
+    },
+    {
+        id: 5,
+        src: "/coke.jpg",
+        threeCardsText1: "paragraph1",
+        threeCardsText2: "paragraph2",
+        threeCardsText3: "paragraph3",
+        text1: "Free sample small tote bag gucci fed...",
+        text2: "#900 - #1,500",
+        text3: "MOQ 4 (pieces)"
+    },
+    {
+        id: 6,
+        src: "/coke.jpg",
+        threeCardsText1: "paragraph1",
+        threeCardsText2: "paragraph2",
+        threeCardsText3: "paragraph3",
+        text1: "Free sample small tote bag gucci fed...",
+        text2: "#900 - #1,500",
+        text3: "MOQ 4 (pieces)"
+    },
+];
+
+export default details;
--- src/components/Three-cards.jsx
+++ src/components/Three-cards.jsx
@@ -3,11 +3,9 @@ import Paragraph from "./Paragraph";
 
 function ThreeCards(props) {
   return (
-    <div>
-        <img src={props.src}/>
-        <Paragraph threeCardsText1={""} text1={"Free sample small tote bag gucci fed..."} text2={"#900 - #1,500"} text3={"MOQ 4 (pieces)"}/>
-        <Paragraph threeCardsText1={""} text1={"Free sample small tote bag gucci fed..."} text2={"#900 - #1,500"} text3={"MOQ 4 (pieces)"}/>
-        <Paragraph threeCardsText1={""} text1={"Free sample small tote bag gucci fed..."} text2={"#900 - #1,500"} text3={"MOQ 4 (pieces)"}/>
+    <div className="three-cards">
+        <img className="prod-img" src={props.src}/>
+        <Paragraph threeCardsText1={props.threeCardsText1} threeCardsText2={props.threeCardsText2} threeCardsText3={props.threeCardsText3} text1={props.text1} text2={props.text2} text3={props.text3}/>
     </div>
   );
 }
