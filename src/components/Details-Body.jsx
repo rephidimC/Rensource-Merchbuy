b42d3e08fde3e6db329a2285f9504f2dd807e136
Victor Adeleke AFOLAYAN <adelekeafolayan@gmail.com>
2021-06-07 22:44:24 +0100

    working on product page

--- src/components/Details-Body.jsx
+++ src/components/Details-Body.jsx
@@ -5,15 +5,31 @@ function DetailsBody(props) {
 
   return (
     <div className="body-details">
-        <div>
-            <img src="" />
+        <div className="body-details-container">
+            <img src="../shopping.png" />
         </div>
-        <div className="round-edge cart-side">
-            <img src="../shopping-cart.png" />
-            <div className="cart-increament">
-                <p>{props.number}</p>
-            </div>
+        <div className="product-details">
+          <p className="product-name">{props.productName}</p>
+          <p className="product-specifics">{props.productDetails}</p>
+          <p className="product-price">{props.productPrice}<span className="product-price-span">/Piece</span></p>
         </div>
+        <div className="thin-line-2"></div>
+        <div className="product-description">
+          <p>Product Description</p>
+          <img src="../left-arrow2.png" />
+        </div>
+        <div className="thin-line-2"></div>
+        <div className="reviews">
+          <p>Reviews and Ratings</p>
+          <p>View all</p>
+        </div>
+        
+        
+        <p></p>
+        <p></p>
+        <p></p>
+        <p></p>
+        <p></p>
     </div>
   );
 }
