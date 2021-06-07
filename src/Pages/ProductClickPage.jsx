fb4608583535e950d09935a8fcd2affdd14bc285
Victor Adeleke AFOLAYAN <adelekeafolayan@gmail.com>
2021-06-07 23:48:16 +0100

    working on products page

--- src/Pages/ProductClickPage.jsx
+++ src/Pages/ProductClickPage.jsx
@@ -6,7 +6,7 @@ function Home() {
   return (
       <div className="container">
       <HeaderDetails heading={"Details"} number={"9"} />
-      <DetailsBody />
+      <DetailsBody productName={"NIKE Huararche 2019"} productDetails={"Get comfy and comfortable with the new 2019 designer sneaker for all your events"} productPrice={"N45,000 - N80,000"} />
 
     </div>
   );
--- src/components/Details-Body.jsx
+++ src/components/Details-Body.jsx
@@ -11,17 +11,19 @@ function DetailsBody(props) {
         <div className="product-details">
           <p className="product-name">{props.productName}</p>
           <p className="product-specifics">{props.productDetails}</p>
-          <p className="product-price">{props.productPrice}<span className="product-price-span">/Piece</span></p>
+          <p className="product-price">{props.productPrice}<span className="product-price-span"> /Piece</span></p>
         </div>
         <div className="thin-line-2"></div>
         <div className="product-description">
           <p>Product Description</p>
-          <img src="../left-arrow2.png" />
+          <img src="../right-arrow.png" />
         </div>
         <div className="thin-line-2"></div>
         <div className="reviews">
-          <p>Reviews and Ratings</p>
-          <p>View all</p>
+          <div className="reviews-first-row">
+            <p>Review and Ratings</p>
+            <p>View all</p>
+          </div>
         </div>
         
         
