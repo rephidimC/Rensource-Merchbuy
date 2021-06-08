33dd3fa9dda57f35205ef31513172505d41011ec
Victor Adeleke AFOLAYAN <adelekeafolayan@gmail.com>
2021-06-08 04:49:54 +0100

    footer edit

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -6,20 +6,29 @@ import Boxes from "../components/Boxes.jsx";
 import ProductCard from "../components/ProductCard.jsx";
 import Creation from "../components/Creation.jsx";
 import Footer from "../components/Footer.jsx";
+import FooterCard from "../components/Footer-Card.jsx";
 
 function Home() {
   return (
       <div className="container">
-      <Header />
-      <div className="thin-line"></div>
-      <HeaderCard />
-      <div className="thin-line"></div>
-      <Input placeholder="Search merchbuy"/>
-      <Boxes />
-      <ProductCard />
-      <Creation />
-      <Footer />
-    </div>
+        <Header />
+        <div className="thin-line"></div>
+        <HeaderCard />
+        <div className="thin-line"></div>
+        <Input placeholder="Search merchbuy"/>
+        <Boxes />
+        <ProductCard />
+        <Creation />
+        <Footer>
+          <div>
+              <FooterCard img={"/dog-house.png"} text={"Home"} />
+              <FooterCard img={"/shopping-cart.png"} text={"Buy"} />
+              <FooterCard img={"/tag.png"} text={"Deals"} />
+              <FooterCard img={"/purse.png"} text={"Wallet"} />
+              <FooterCard img={"/menu.png"} text={"More"} />
+          </div>
+        </Footer>    
+      </div>
   );
 }
 
